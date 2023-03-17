2d9fb43234fff2599d2b29a2b1ef78245a9d30cd
Denis <109104246+Denis0014@users.noreply.github.com>
2023-03-17 20:47:16 +0300

    Задание 1

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace HomeWork3
 {
     internal class Program
     {
+        static void PrintArr<T>(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+        }
+        static string[] GetRegMarks(string s) => Regex.Matches(s, @"[АВЕКМНОРСТУХ]{2}\d{3}[АВЕКМНОРСТУХ]").Select(x => x.Value).ToArray();
         static void Main(string[] args)
         {
-            
+            PrintArr(GetRegMarks("ХХ001Х АО101К НО712Е"));
         }
     }
 } 
